c099e9407b3c331c6490578171594543ff060c40
audiesha brattin <a.brattin@gmail.com>
2021-04-27 15:41:34 -0500

    add all

---
+++ src/main/java/com/audib/audispring/AudiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.audib.audispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AudiSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/audib/audispring/AudiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audib.audispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
